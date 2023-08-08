b36f8ef18dcca62083a001eae947b8e289e28025
Richardt Joergensen <richardt.joergensen@nerdware.dev>
2023-08-08 11:53:36 +0200

    1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # debugging-in-python
+This project is made to help the employees at nerdware to learn more about using the debugger to debug their code. 
+Inside the src folder is a python file, which can be used to test the debugger fuctions.
+
+There is already a lauchn.json file, which should be used as configuration for the debugger.
---
+++ src/example.py
@@ -0,0 +1,31 @@
+import random
+
+
+def average(d,n):
+    avg = d/n
+    return avg
+
+
+def running_average(numbers):
+    avgs = []
+    total = 0
+    for i, num in enumerate(numbers):
+        total += num
+        current_avg = average(total, i+1)
+        avgs.append(current_avg)
+   
+    return avgs
+
+def highest_running_average(numbers):
+    avgs = running_average(numbers)
+    highest = max(avgs)
+    return highest
+
+def generate_numbers(n):
+    return random.sample(range(1, 1000), n)
+
+if __name__ == '__main__':
+    numbers = generate_numbers(100)
+    print("Numbers: ", numbers)
+    print("Running average: ", running_average(numbers))
+    print("Highest running average: ", highest_running_average(numbers))
